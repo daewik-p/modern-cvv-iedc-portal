3df2b4155c44d770d23727ebe53e6dd991847cf2
daewik-p <daewikprasheen@gmail.com>
2025-02-26 17:42:06 +0530

    building test

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/modern-cvv-iedc-portal/',
+
   server: {
     host: "::",
     port: 8080,
