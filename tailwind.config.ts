2288d77c207f10d4d71e68ce409c2df338ebffc2
daewik-p <daewikprasheen@gmail.com>
2025-02-26 14:36:41 +0530

    `Refactor navigation links and hero images, update styles`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,7 +26,7 @@ export default {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "#9b87f5",
+          DEFAULT: "#0a192f",
           foreground: "#FFFFFF",
         },
         secondary: {
